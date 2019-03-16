5bfd058ffdc762b509994bc54c35e44a7a55e875
Chen Lin <chen45464546@163.com>
2019-03-16 22:40:03 +0800

    makefile sample

---
+++ app/makefile_test/sample/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ #include "math.h"
+ int main()
+ {
+ int sum = 0;
+ sum = add(4, 3);
+ printf("sum:%d", sum);
+ 
+int result = 0;
+ result = divide(4, 0);
+ printf("result:%d\n", result);
+ return 0;
+ }
